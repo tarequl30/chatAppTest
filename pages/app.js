c4d540b04ef03acfe770617c1d6680362ec3646a
apple <apple@apples-MacBook-Pro.local>
2021-12-06 15:53:00 +0600

    changes

--- pages/app.js
+++ pages/app.js
@@ -3,8 +3,6 @@ import "../styles/auth.css"
 import "../styles/chats.css"
 import "../styles/index.css"
 
-// import { ContextProvider } from '../context'
-
 export default function App({ Component, pageProps }) {
   return (
     <ContextProvider>
